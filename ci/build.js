5b3779ee66593998f4e4c659ae41a031a1894952
Mark J. Becker <herzog31@users.noreply.github.com>
2018-08-27 12:51:54 +0200

    Cross Platform Support (#40)
    * trivial - Execute serverless permission fix only on CI
    * trivial - Make deployment-install.js compatible with Windows
    * trivial - Remove npm bin
    * trivial - Fix serverless permission
    * trivial - Configure Mocha without environment variable
    * trivial - Use forked eslint-plugin-header version with Windows fix
    * trivial - Fix path in tests

--- ci/build.js
+++ ci/build.js
@@ -46,6 +46,9 @@ if ("test-it" in pkg.scripts && process.env.CORE_WSK_AUTH_STRING) {
     process.env.OW_PACKAGE_SUFFIX = `magento-${branch}-${buildNum}`;
 
     try {
+        // Fix serverless permissions
+        ci.sh('$(npm bin)/lerna run fix-serverless-permission');
+
         ci.withWskCredentials(process.env.WSK_API_HOST, process.env.CORE_WSK_NAMESPACE, process.env.CORE_WSK_AUTH_STRING, () => {
             ci.sh('$(npm bin)/lerna run deploy-suffix --concurrency 1');
         });
@@ -55,6 +58,10 @@ if ("test-it" in pkg.scripts && process.env.CORE_WSK_AUTH_STRING) {
                 ci.withWskCredentials(process.env.WSK_API_HOST, process.env.CUSTOMER_WSK_NAMESPACE, process.env.CUSTOMER_WSK_AUTH_STRING, () => {
                     ci.withCredentials(process.env.BACKEND_CREDENTIALS, () => {
                         ci.sh('npm install');
+
+                        // Fix serverless permissions
+                        ci.sh('npm run fix-serverless-permission');
+
                         let params = '--customer-package magento@' + process.env.OW_PACKAGE_SUFFIX + ' --customer-namespace ' + process.env.CUSTOMER_WSK_NAMESPACE + ' --bindings-namespace ' + process.env.CORE_WSK_NAMESPACE;
                         ci.sh('$(npm bin)/serverless deploy ' + params);
                     });
