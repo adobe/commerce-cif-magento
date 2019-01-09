146f220f3d5198ba9b5a43b11da2583ee9e49c96
Christophe Jelger <jelger@adobe.com>
2019-01-09 14:11:55 +0100

    CIF-473 - [Auth] Implement authorization endpoints for guest/anonymous login (#73)
    - Magento does not support guest tokens, so the implementation basically returns HTTP 501

---
+++ test/customer/postCustomerAuthTest.js
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ *
+ *    Copyright 2018 Adobe. All rights reserved.
+ *    This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License. You may obtain a copy
+ *    of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language
+ *    governing permissions and limitations under the License.
+ *
+ ******************************************************************************/
+
+'use strict';
+
+const assert = require('chai').assert;
+const setup = require('../lib/setupTest').setup;
+
+describe('Magento postCustomerAuth', () => {
+    describe('Unit tests', () => {
+
+        // Add helpers to context
+        setup(this, __dirname, 'postCustomerAuth');
+
+        it('returns a not implemented error for guest authentication', () => {
+            return this.execute({
+                type: 'guest'
+            }).then(result => {
+                assert.strictEqual(result.response.error.name, 'NotImplementedError');
+            });
+        });
+
+        it('returns invalid argument error for bad type parameter', () => {
+            return this.execute({
+                type: 'test'
+            }).then(result => {
+                assert.strictEqual(result.response.error.name, 'InvalidArgumentError');
+            });
+        });
+
+        it('returns missing property error for missing email parameter', () => {
+            return this.execute({
+                type: 'credentials',
+                password: '1234'
+            }).then(result => {
+                assert.strictEqual(result.response.error.name, 'MissingPropertyError');
+            });
+        });
+
+        it('returns missing property error for missing password parameter', () => {
+            return this.execute({
+                type: 'credentials',
+                email: 'john.doe@whatever.com'
+            }).then(result => {
+                assert.strictEqual(result.response.error.name, 'MissingPropertyError');
+            });
+        });
+    });
+});
